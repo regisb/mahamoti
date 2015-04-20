ff6efa1be9d8bab5fbabdf4cf149cedb8e8a4834
Régis Behmo <regis@behmo.com>
2015-04-20 09:11:31 +0200

    DateTime parsing courtesy of dateutil

--- mahamoti/forms.py
+++ mahamoti/forms.py
@@ -1,11 +1,21 @@
+from datetime import datetime
+import wtforms.ext.dateutil.fields
 import wtforms.fields
 import wtforms.form
 import wtforms.validators
 
+class BaseForm(wtforms.Form):
+    name = "Override this in your forms"
+
 class TimestampedForm(wtforms.Form):
-    time = wtforms.fields.DateTimeField("Time")
+    time = wtforms.ext.dateutil.fields.DateTimeField(
+        "Time",
+        default=datetime.now(),
+        validators=[wtforms.validators.Required()]
+    )
 
 class RunningForm(TimestampedForm):
+    name = "Running"
     distance = wtforms.fields.FloatField("Distance", validators=[wtforms.validators.NumberRange(0)])
 
 def instances():
